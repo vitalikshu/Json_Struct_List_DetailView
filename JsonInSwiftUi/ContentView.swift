71a462263be99c447e3124a19411b19e5bbac198
vitalikshu <37334718+vitalikshu@users.noreply.github.com>
2021-02-04 13:04:38 +0200

    Initial Commit

---
+++ JsonInSwiftUi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JsonInSwiftUi
+//
+//  Created by PROG on 04/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JsonInSwiftUi/JsonInSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonInSwiftUiApp.swift
+//  JsonInSwiftUi
+//
+//  Created by PROG on 04/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct JsonInSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
